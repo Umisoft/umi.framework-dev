b4b6d0136742a1a65656fd89298be496c5a73a69
Daria Prusova <guzhova@umisoft.ru>
2014-02-19 19:18:02 +0400

    [orm] add IHierarchicCollection::selectByUri()

--- tests/utest/orm/func/collection/simplehierarchic/SimpleHierarchicCollectionTest.php
+++ tests/utest/orm/func/collection/simplehierarchic/SimpleHierarchicCollectionTest.php
@@ -12,10 +12,28 @@ namespace utest\orm\func\collection\simplehierarchic;
 use umi\orm\collection\ICollectionFactory;
 use umi\orm\collection\SimpleHierarchicCollection;
 use umi\orm\metadata\IObjectType;
+use umi\orm\object\IHierarchicObject;
 use utest\orm\ORMDbTestCase;
 
 class SimpleHierarchicCollectionTest extends ORMDbTestCase
 {
+
+    /**
+     * @var SimpleHierarchicCollection $collection
+     */
+    private $collection;
+    /**
+     * @var IHierarchicObject $object объект коллекции
+     */
+    private $object;
+    /**
+     * @var IHierarchicObject $object2 объект коллекции
+     */
+    private $object2;
+    /**
+     * @var IHierarchicObject $wrongObject объект чужой коллекции
+     */
+    private $wrongObject;
 
     /**
      * {@inheritdoc}
@@ -33,49 +51,66 @@ class SimpleHierarchicCollectionTest extends ORMDbTestCase
                     'class'     => 'utest\orm\mock\collections\BlogsCollection',
                     'hierarchy' => self::SYSTEM_HIERARCHY
                 ],
+                self::BLOGS_POST             => [
+                    'type'      => ICollectionFactory::TYPE_LINKED_HIERARCHIC,
+                    'hierarchy' => self::SYSTEM_HIERARCHY
+                ],
+                self::USERS_USER             => [
+                    'type' => ICollectionFactory::TYPE_SIMPLE
+                ],
+                self::USERS_GROUP            => [
+                    'type' => ICollectionFactory::TYPE_SIMPLE
+                ],
                 self::SYSTEM_MENU            => [
                     'type' => ICollectionFactory::TYPE_SIMPLE_HIERARCHIC
                 ]
             ],
-            false
+            true
         ];
     }
 
-    public function testMethods()
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUpFixtures()
+    {
+        $this->collection = $this->getCollectionManager()->getCollection(self::SYSTEM_MENU);
+        $this->object = $this->collection->add('menuItem1');
+        $this->object2 = $this->collection->add('menuItem2', IObjectType::BASE, $this->object);
+        $this->wrongObject = $this->getCollectionManager()->getCollection(self::BLOGS_BLOG)->add('blog1');
+    }
+
+    public function testAdd()
     {
 
-        /**
-         * @var SimpleHierarchicCollection $collection
-         */
-        $collection = $this->getCollectionManager()->getCollection(self::SYSTEM_MENU);
-
-        $object = $collection->add('menuItem1');
         $this->assertInstanceOf(
             'umi\orm\object\IHierarchicObject',
-            $object,
+            $this->object,
             'Ожидается, что метод IHierarchicCollection::add() вернет IHierarchicObject'
         );
         $this->assertEquals(
             IObjectType::BASE,
-            $object->getTypeName(),
+            $this->object->getTypeName(),
             'Ожидается, что по умолчанию добавляется объект базового типа'
         );
-        $this->assertNull($object->getParent(), 'Ожидается, что по умолчанию объект был добавлен у корень иерархии');
+        $this->assertNull($this->object->getParent(), 'Ожидается, что по умолчанию объект был добавлен у корень иерархии');
 
-        $object2 = $collection->add('menuItem2', IObjectType::BASE, $object);
         $this->assertTrue(
-            $object === $object2->getParent(),
+            $this->object === $this->object2->getParent(),
             'Ожидается, что объект был созданным с заданным родителем'
         );
         $this->assertEquals(
             1,
-            $object->getChildCount(),
+            $this->object->getChildCount(),
             'Ожидается, что при добавлении объекта у его родителя возрастет количество детей'
         );
+    }
 
+    public function testImpossibleAdd()
+    {
         $e = null;
         try {
-            $collection->add('menuItem3', 'not_existing_type');
+            $this->collection->add('menuItem3', 'not_existing_type');
         } catch (\Exception $e) {
         }
         $this->assertInstanceOf(
@@ -84,18 +119,9 @@ class SimpleHierarchicCollectionTest extends ORMDbTestCase
             'Ожидается, что нельзя добавить объект несуществующего типа'
         );
 
-        $wrongObject = $this->getCollectionManager()->getCollection(self::BLOGS_BLOG)
-            ->add('blog1');
-
-        $this->assertTrue($collection->contains($object), 'Ожидается, что коллекция содержит созданный в ней объект');
-        $this->assertFalse(
-            $collection->contains($wrongObject),
-            'Ожидается, что коллекция не содержит объект, коллекция которого не равна этой коллекции'
-        );
-
         $e = null;
         try {
-            $collection->add('menuItem4', IObjectType::BASE, $wrongObject);
+            $this->collection->add('menuItem4', IObjectType::BASE, $this->wrongObject);
         } catch (\Exception $e) {
         }
         $this->assertInstanceOf(
@@ -103,10 +129,27 @@ class SimpleHierarchicCollectionTest extends ORMDbTestCase
             $e,
             'Ожидается, что при добавлении объекта нельзя выставить в качестве родителя объект другой коллекции'
         );
+    }
 
+    public function testContains()
+    {
+        $this->assertTrue(
+            $this->collection->contains($this->object),
+            'Ожидается, что коллекция содержит созданный в ней объект'
+        );
+
+        $this->assertFalse(
+            $this->collection->contains($this->wrongObject),
+            'Ожидается, что коллекция не содержит объект, коллекция которого не равна этой коллекции'
+        );
+
+    }
+
+    public function testDelete()
+    {
         $e = null;
         try {
-            $collection->delete($wrongObject);
+            $this->collection->delete($this->wrongObject);
         } catch (\Exception $e) {
         }
         $this->assertInstanceOf(
@@ -117,13 +160,35 @@ class SimpleHierarchicCollectionTest extends ORMDbTestCase
 
         $this->assertInstanceOf(
             'umi\orm\collection\IHierarchicCollection',
-            $collection->delete($object2),
+            $this->collection->delete($this->object2),
             'Ожидается, что метод ISimpleCollection::delete() вернет ISimpleCollection'
         );
         $this->assertEquals(
             0,
-            $object->getChildCount(),
+            $this->object->getChildCount(),
             'Ожидается, что при удалении объекта у его родителя уменьшится количество детей'
+        );
+    }
+
+    public function testGetByUri()
+    {
+        $this->getObjectPersister()->commit();
+
+        $this->assertInstanceOf(
+            'umi\orm\object\IHierarchicObject',
+            $this->collection->getByUri('/menuItem1/menuItem2'),
+            'Ожидается, что метод IHierarchicCollection::getByUri() вернет IHierarchicObject'
+        );
+
+        $e = null;
+        try {
+            $this->collection->getByUri('/wrongUri');
+        } catch (\Exception $e) {
+        }
+        $this->assertInstanceOf(
+            'umi\orm\exception\NonexistentEntityException',
+            $e,
+            'Ожидается, исключение, если объект не был найден по URI'
         );
 
     }
